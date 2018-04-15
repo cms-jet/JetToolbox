a945baec786d60573ea70df0bf8c05e9e8f1feaf
Alejandro <gomez@physics.rutgers.edu>
2018-04-15 09:43:08 +0200

    working example

--- test/jettoolbox_cfg.py
+++ test/jettoolbox_cfg.py
@@ -1,17 +1,17 @@
 import FWCore.ParameterSet.Config as cms
-
-process = cms.Process('jetToolbox')
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('jetToolbox', eras.Run2_2017)
 
 process.load("Configuration.EventContent.EventContent_cff")
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load('Configuration.Geometry.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_v4'
+process.GlobalTag.globaltag = '92X_upgrade2017_realistic_v1'
 
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilter','manystripclus53X','toomanystripclus53X')
+#process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilter','manystripclus53X','toomanystripclus53X')
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
 
@@ -24,7 +24,19 @@ process.options.allowUnscheduled = cms.untracked.bool(True)
 #######################################################################
 
 from JMEAnalysis.JetToolbox.jetToolbox_cff import jetToolbox
-#jetToolbox( process, 'ak4', 'jetSequence', 'out', PUMethod='Puppi', miniAOD=True, runOnMC=False )
+
+
+jetToolbox( process, 'ak8', 'jetSequence', 'out', PUMethod='CHS', miniAOD=True, 
+		#Cut='pt > 200 && abs(eta) < 2.5', # Tight
+		runOnMC=False,
+		addPruning = True, addSoftDrop = True, 
+		addNsub    = True, 
+		#addPrunedSubjets=True, addSoftDropSubjets=True,  
+		#addNsubSubjets  =True 
+		)
+
+'''
+#jetToolbox( process, 'ak4', 'jetSequence', 'out', PUMethod='Puppi', miniAOD=True, runOnMC=True )
 #process.load('CommonTools.PileupAlgos.Puppi_cff')
 #process.PuppiOnTheFly = process.puppi.clone( candName = cms.InputTag( 'packedPFCandidates' ), vertexName = cms.InputTag( 'offlineSlimmedPrimaryVertices' ), clonePackedCands = cms.bool(True) )
 #process.PuppiOnTheFly.useExistingWeights = True
@@ -38,11 +50,11 @@ from JMEAnalysis.JetToolbox.jetToolbox_cff import jetToolbox
 #jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', PUMethod='CHS', addPruning=True, addSoftDrop=True , addSoftDropSubjets=True,  addNsub=True, maxTau=6, addTrimming=True, addFiltering=True, addNsubSubjets=True ) 
 
 listBtagDiscriminatorsAK4 = [ 
-                'pfJetProbabilityBJetTags',
-                'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+                #'pfJetProbabilityBJetTags',
+                #'pfCombinedInclusiveSecondaryVertexV2BJetTags',
                 'pfCombinedMVAV2BJetTags',
-                'pfCombinedCvsLJetTags',
-                'pfCombinedCvsBJetTags',
+                #'pfCombinedCvsLJetTags',
+                #'pfCombinedCvsBJetTags',
                 ]
 jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', 
 		PUMethod='CHS',
@@ -50,19 +62,20 @@ jetToolbox( process, 'ak8', 'ak8JetSubs', 'out',
 		JETCorrPayload='AK8PFchs', 
 		#addEnergyCorrFunc=True, 
 		#updateCollectionSubjets='slimmedJetsAK8PFCHSSoftDropPacked:SubJets', 
-		subJETCorrPayload='AK4PFchs', 
+		#subJETCorrPayload='AK4PFchs', 
 		bTagDiscriminators=listBtagDiscriminatorsAK4, 
 		#addNsubSubjets=True, 
-		addPrunedSubjets=True,
+		#addPrunedSubjets=True,
 		postFix= 'CollectionTests',
-		addTrimming=True, 
-		addPruning=True 
+		#addTrimming=True, 
+		#addPruning=True 
 		)  #, addPrunedSubjets=True, subJETCorrPayload='AK4PFchs' ) 
+
 jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', 
 		PUMethod='CHS',
 		#updateCollection='slimmedJetsAK8', 
 		JETCorrPayload='AK8PFchs', 
-		addEnergyCorrFunc=True, 
+		#addEnergyCorrFunc=True, 
 		#updateCollectionSubjets='slimmedJetsAK8PFCHSSoftDropPacked:SubJets', 
 		subJETCorrPayload='AK4PFchs', 
 		bTagDiscriminators=listBtagDiscriminatorsAK4, 
@@ -77,8 +90,8 @@ jetToolbox( process, 'ak8', 'ak8JetSubs', 'out',
 #jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', PUMethod='CHS', JETCorrPayload = 'AK8PFchs', addEnergyCorrFunc=True, addNsubSubjets=True, addTrimming=True, addFiltering=True, addPruning=True, addSoftDrop=True, addSoftDropSubjets=True )  #PUMethod='CHS', addPrunedSubjets=True, subJETCorrPayload='AK4PFchs' ) 
 
 
-jetToolbox( process, 'ak4', 'ak4JetSubsUpdate', 'out', updateCollection='slimmedJets', JETCorrPayload = 'AK4PFchs', JETCorrLevels=['L2Relative', 'L3Absolute'], addQGTagger=True, addPUJetID=True, bTagDiscriminators=listBtagDiscriminatorsAK4 ) 
-jetToolbox( process, 'ak4', 'ak4JetSubs', 'out', addQGTagger=True, addPUJetID=True, postFix='New' ) 
+#jetToolbox( process, 'ak4', 'ak4JetSubsUpdate', 'out', updateCollection='slimmedJets', JETCorrPayload = 'AK4PFchs', JETCorrLevels=['L2Relative', 'L3Absolute'], addQGTagger=True, addPUJetID=True, bTagDiscriminators=listBtagDiscriminatorsAK4 ) 
+#jetToolbox( process, 'ak4', 'ak4JetSubs', 'out', addQGTagger=True, addPUJetID=True, postFix='New' ) 
 
 
 #jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', PUMethod='CHS', addPruning=True, addSoftDrop=True , addPrunedSubjets=True, addSoftDropSubjets=True, addNsub=True, maxTau=6, addTrimming=True, addFiltering=True, miniAOD=False ) 
@@ -110,24 +123,25 @@ jetToolbox( process, 'ak4', 'ak4JetSubs', 'out', addQGTagger=True, addPUJetID=Tr
 #jetToolbox( process, 'ca12', 'ca12JetSubs', 'out', PUMethod='Puppi', addHEPTopTagger=True, addSoftDrop=True, miniAOD=False )
 #jetToolbox( process, 'ca12', 'ca12JetSubs', 'out', PUMethod='SK', addHEPTopTagger=True, addSoftDrop=True, miniAOD=False )
 #jetToolbox( process, 'ca12', 'ca12JetSubs', 'out', addHEPTopTagger=True, addSoftDrop=True, miniAOD=False )
+'''
 
 
 
-process.endpath = cms.EndPath(process.out)
 
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
 process.source = cms.Source("PoolSource",
 #		fileNames = cms.untracked.vstring(#'file:example.root'
 		fileNames = cms.untracked.vstring(
-			'/store/data/Run2016G/JetHT/MINIAOD/PromptReco-v1/000/280/002/00000/E82E26C7-4375-E611-AE7F-FA163E48F736.root'
-	#		'/store/mc/RunIISpring15DR74/WWTo2L2Nu_13TeV-powheg/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/60000/0AA28275-5001-E511-8A45-0CC47A4DEDE0.root'
-#		#	'/store/relval/CMSSW_7_4_0_pre9_ROOT6/RelValQCD_Pt_600_800_13/MINIAODSIM/MCRUN2_74_V7-v1/00000/281DDBC1-D3D1-E411-8534-0025905AA9F0.root',
-#		#	'/store/relval/CMSSW_7_4_0_pre9_ROOT6/RelValQCD_Pt_600_800_13/MINIAODSIM/MCRUN2_74_V7-v1/00000/C01855C8-D3D1-E411-A702-0026189438D7.root'
-			)
+#			'/store/data/Run2016G/JetHT/MINIAOD/PromptReco-v1/000/280/002/00000/E82E26C7-4375-E611-AE7F-FA163E48F736.root'
+#			'/store/mc/RunIISpring15DR74/WWTo2L2Nu_13TeV-powheg/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/60000/0AA28275-5001-E511-8A45-0CC47A4DEDE0.root'
+#			'/store/relval/CMSSW_7_4_0_pre9_ROOT6/RelValQCD_Pt_600_800_13/MINIAODSIM/MCRUN2_74_V7-v1/00000/281DDBC1-D3D1-E411-8534-0025905AA9F0.root',
+#			'/store/relval/CMSSW_7_4_0_pre9_ROOT6/RelValQCD_Pt_600_800_13/MINIAODSIM/MCRUN2_74_V7-v1/00000/C01855C8-D3D1-E411-A702-0026189438D7.root'
+			#'/store/mc/RunIIFall17MiniAOD/VectorDiJet1Jet_madgraph_Mphi50Mchi1_13TeV/MINIAODSIM/94X_mc2017_realistic_v11-v1/00000/64BF8D7C-C310-E811-ACBE-A0369FC5E9A4.root'
+			'/store/relval/CMSSW_9_4_5_cand1/JetHT/MINIAOD/94X_dataRun2_relval_v11_RelVal_rmaod_jetHT2017B-v1/10000/18B5E95F-992E-E811-9422-0CC47A78A418.root'
+			),
 		)
-from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValTTbarPileUpMINIAODSIM
-process.source.fileNames = filesRelValTTbarPileUpMINIAODSIM
+#from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValTTbarPileUpMINIAODSIM
+#process.source.fileNames = filesRelValTTbarPileUpMINIAODSIM
 
 #from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValProdTTbarAODSIM
 #process.source.fileNames = filesRelValProdTTbarAODSIM
